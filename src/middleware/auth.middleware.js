1fd4d225bd627afe4441b484fbe5732b30e79b6f
singhak <anilsingh.singh09@gmail.com>
2023-12-19 09:10:38 +0530

    logic update in verify JWT

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,20 +4,17 @@ import { asyncHandler } from "../utils/asyncHandler.utils.js";
 import jwt from "jsonwebtoken";
 
 const VerifyJWT = asyncHandler(async (req, res, next) => {
-    try {
-        let accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-        let decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+    let accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+    let decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
 
-        if (!accessToken) throw new ApiError('401', "Unautorized Access");
+    if (!accessToken) throw new ApiError('401', "Unautorized Access");
 
-        let user = await User.findById({ _id: decodedToken._id }).select("-password -refreshToken");
-        if (!user) throw new ApiError(401, "Invalid Access Token");
-        req.user = user;
-        next();
-    } catch (error) {
-        console.log("VerifyJWT: Error: ", error);
-        throw new ApiError(401, error?.message || "Invalid Access Token");
-    }
+    let user = await User.findById({ _id: decodedToken._id }).select("-password");
+    if (!user) throw new ApiError(401, "Invalid Access Token");
+    // if refresh token not exist, it means we logout the user and user is using old accesstoken
+    if (!user?.refreshToken) throw new ApiError(401, "Invalid Access Token");
+    req.user = user;
+    next();
 })
 
 export { VerifyJWT }
